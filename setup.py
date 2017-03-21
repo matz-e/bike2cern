0d8418d71a35b2eae405a5434b61b065873e433f
Matthias Wolf <matthias@sushinara.net>
2017-03-21 14:16:36 +0100

    Update metadata.

--- setup.py
+++ setup.py
@@ -4,14 +4,20 @@ from setuptools import setup
 
 setup(
     name='bike2cern',
-    version=0.1,
+    version=0.1.1,
     description='Sync rides from Strava to CERN Sharepoint',
     author='Matthias Wolf',
     author_email='m@sushinara.net',
     url='https://github.com/matz-e/bike2cern',
-    download_url='https://github.com/matz-e/bike2cern/archive/0.1.tar.gz',
+    download_url='https://github.com/matz-e/bike2cern/archive/0.1.1.tar.gz',
     keywords=['strava'],
-    classifiers=[],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
     packages=['bike2cern'],
     install_requires=[
         'keyring',
